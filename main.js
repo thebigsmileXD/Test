a38ae2af71c89cd7a4198a2e9f1148f3f2c0e1e9
rom_314 <town_314159265358979@yahoo.co.jp>
2019-11-11 20:19:34 +0900

    drop down update v2 at 2019/11/11 20:19

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function manifest() {
 
 // ドロップダウンメニュー
 $(function() {
+    // ドロップダウンメニュー
     $('.dropdown-btn').hover(
         function() {
             //カーソルが重なった時
@@ -23,6 +24,22 @@ $(function() {
             $(this).children('.dropdown').removeClass('open');
         }
     );
+    // グローバルナビの開閉
+    $(function() {
+        $('.nav-button-wrap').on('click', function() {
+            if ($(this).hasClass('active')) {
+                // スマホ用メニューが表示されていたとき
+                $(this).removeClass('active');
+                $('.globalnav').addClass('close');
+                $('.globalnav-wrap , body').removeClass('open');
+            } else {
+                // スマホ用メニューが非表示の時
+                $(this).addClass('active');
+                $('.globalnav').removeClass('close');
+                $('.globalnav-wrap , body').addClass('open');
+            }
+        });
+    });
 });
 
 
